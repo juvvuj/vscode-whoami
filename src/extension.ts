38be319c6de4b3dd30d96e9f7b5ac9044f78b987
juvvuj <juvvuj@gmail.com>
2021-10-01 11:25:58 -0400

    ext: add initial command implementation
    Signed-off-by: juvvuj <juvvuj@gmail.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,14 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-    let disposable = vscode.commands.registerCommand('vscode-whoami.echoName', () => {
-        vscode.window.showInformationMessage('Hello World from vscode-whoami!');
+    let disposable = vscode.commands.registerCommand('vscode-whoami.echoName', async () => {
+        const name = await vscode.window.showInputBox({
+            title: 'Full Name',
+            prompt: 'Please enter your full name'
+        });
+        if (name) {
+            vscode.window.showInformationMessage(`Hello ${name}!`);
+        }
     });
 
     context.subscriptions.push(disposable);
